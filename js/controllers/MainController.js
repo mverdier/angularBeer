ece9c47cd8066539c1953852db676bc3cc21f78a
mverdier <mart.verdier@gmail.com>
2016-11-10 10:57:09 +0100

    Updated results to cards

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -24,6 +24,58 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		"taste": "sugar",
 		"picture": "resource/beer/liefmans_fruitesse.png",
 		"description": "This is a description"
+	},
+
+	{
+		"name": "Liefmans Fruitesse",
+		"brand": "Liefmans",
+		"abv": 3.8,
+		"color": "red",
+		"flavor": "fruit",
+		"country": "BE",
+		"countryLong": "Belgium",
+		"taste": "sugar",
+		"picture": "resource/beer/liefmans_fruitesse.png",
+		"description": "Sipping a Liefmans Fruitesse on the rocks is like taking a city break in Berlin, soaking up the atmosphere in Milan or discovering culture in New York. Take your time to enjoy it in a friendly bar, or chilling out with your friends."
+	},
+
+	{
+		"name": "Liefmans Fruitesse",
+		"brand": "Liefmans",
+		"abv": 3.8,
+		"color": "red",
+		"flavor": "fruit",
+		"country": "BE",
+		"countryLong": "Belgium",
+		"taste": "sugar",
+		"picture": "resource/beer/liefmans_fruitesse.png",
+		"description": "Sipping a Liefmans Fruitesse on the rocks is like taking a city break in Berlin, soaking up the atmosphere in Milan or discovering culture in New York. Take your time to enjoy it in a friendly bar, or chilling out with your friends."
+	},
+
+	{
+		"name": "Liefmans Fruitesse",
+		"brand": "Liefmans",
+		"abv": 3.8,
+		"color": "red",
+		"flavor": "fruit",
+		"country": "BE",
+		"countryLong": "Belgium",
+		"taste": "sugar",
+		"picture": "resource/beer/liefmans_fruitesse.png",
+		"description": "Sipping a Liefmans Fruitesse on the rocks is like taking a city break in Berlin, soaking up the atmosphere in Milan or discovering culture in New York. Take your time to enjoy it in a friendly bar, or chilling out with your friends."
+	},
+
+	{
+		"name": "Liefmans Fruitesse",
+		"brand": "Liefmans",
+		"abv": 3.8,
+		"color": "red",
+		"flavor": "fruit",
+		"country": "BE",
+		"countryLong": "Belgium",
+		"taste": "sugar",
+		"picture": "resource/beer/liefmans_fruitesse.png",
+		"description": "Sipping a Liefmans Fruitesse on the rocks is like taking a city break in Berlin, soaking up the atmosphere in Milan or discovering culture in New York. Take your time to enjoy it in a friendly bar, or chilling out with your friends."
 	}];
 
 	$scope.search = function() {
